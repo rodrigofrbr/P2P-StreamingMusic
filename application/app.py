088d1e742713670a32398a246bb0abca9b96ac8e
rodrigo <rodrigofr@id.uff.br>
2025-01-07 22:17:21 -0300

    implementacaoStreamDocker

--- application/app.py
+++ application/app.py
@@ -1,36 +1,35 @@
 import json
 import socket
+import threading
+from tabulate import tabulate
 from models.client import Client
 from models.file import File
+import wave
 
-from tabulate import tabulate
-
-def start_client():
+def client_service():
     action = input("Escreva o par exclusivo 'IP:porta' para acessar o proxy: ")
-    ip_adress, port = [action.split(':')[0], action.split(':')[1]]
-    _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    _socket.connect((ip_adress, int(port)))  
+    proxy_ip, proxy_port = action.split(':')
+    _proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    _proxy_socket.connect((proxy_ip, int(proxy_port)))
 
-    is_not_registered : bool = json.loads(_socket.recv(1024).decode())
+    is_not_registered = json.loads(_proxy_socket.recv(1024).decode())
 
-    if(is_not_registered):
+    if is_not_registered:
         print("Cliente não registrado.")
         username = input("Informe seu nome de usuário para continuar: ")
 
         new_client_data = Client(username, "", [], True)
-        
         new_client = Client.to_json(new_client_data)
-        _socket.send(json.dumps(new_client).encode())
+        _proxy_socket.send(json.dumps(new_client).encode())
     else:
         print("Cliente registrado.")
 
     while True:
-        action = input("Digite 'LISTA' para ver os arquivos, 'ADD_FILE' para adicionar arquivos ou 'FIM' para encerrar: ")
-        _socket.send(json.dumps(action).encode())
-        
+        action = input("Digite 'LISTA' para ver os arquivos, 'ADD_FILE' para adicionar arquivos, 'CONNECT_PEER' para obter o streaming do áudio ou 'FIM' para encerrar: ")
+        _proxy_socket.send(json.dumps(action).encode())
+
         if action == "LISTA":
-            
-            client_list_data = json.loads(_socket.recv(1024).decode())
+            client_list_data = json.loads(_proxy_socket.recv(1024).decode())
             client_list = [Client.from_json(client_data) for client_data in client_list_data]
 
             for client in client_list:
@@ -43,21 +42,110 @@ def start_client():
                 ]
                 print(tabulate(client_table, headers="firstrow", tablefmt="grid"))
                 print()
-            
+
         elif action == "ADD_FILE":
             file_name = input("Informe o nome do arquivo: ")
             file_path = input("Informe o caminho desse arquivo: ")
             file_data = File.to_json(File(file_name, file_path))
 
-            _socket.send(json.dumps(file_data).encode())
-            print(_socket.recv(1024).decode())
+            _proxy_socket.send(json.dumps(file_data).encode())
+            print(_proxy_socket.recv(1024).decode())
+
+        elif action == "CONNECT_PEER":
+            udp_socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            udp_socket_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             
+            ip_peer = input("Informe o endereço IP do peer: ")
+            file_name_peer = input("Informe o nome do arquivo do peer: ")
+
+            udp_socket_client.sendto(file_name_peer.encode(), (ip_peer, 4200))
+            print("Recebendo áudio e enviando para agente...")
+
+            try:
+                while True:
+                    # Recebe os dados de áudio em blocos
+                    data, _ = udp_socket_client.recvfrom(1024)
+                    print("Recebendo e processando stream para agente de áudio...")
+                    if data == b"END_OF_FILE":
+                        break
+
+                    # Especificamente para tratar a execucao em tempo real no docker
+                    udp_socket_send.sendto(data, ('host.docker.internal', 4200))
+
+                    try:
+                        ack, _ = udp_socket_send.recvfrom(1024)
+                        if ack != b"ACK":
+                            udp_socket_send.sendto(data, ('host.docker.internal', 4200))
+                            continue
+                    except socket.timeout:
+                        print("Timeout aguardando ACK. Reenviando pacote.")
+                        udp_socket_send.sendto(data, ('host.docker.internal', 4200))
+
+                    udp_socket_client.sendto(b"ACK", (ip_peer, 4201))
+
+            except Exception as e:
+                print(f"Ocorreu um erro ao receber o arquivo '{file_name_peer}': {e}")
+
+            udp_socket_client.close()
+            udp_socket_send.close()
+            print("Stream de áudio concluído.")
+
         elif action == "FIM":
             print("Encerrando conexão...")
-            response = _socket.recv(1024).decode()
+            response = _proxy_socket.recv(1024).decode()
             print(response)
             break
 
-    _socket.close()
+def start_client():
+    udp_socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket_server.bind(("0.0.0.0", 4200))
+
+    thread = threading.Thread(target=client_service)
+    thread.start()
+
+    while True:
+        data, client_address = udp_socket_server.recvfrom(1024)
+        print(f"Solicitação recebida de: {client_address} para o arquivo: {data.decode()}")
+
+        thread = threading.Thread(target=handle_udp_conn, args=(data, client_address))
+        thread.start()
+
+def handle_udp_conn(data, client_address):
+    file_name = data.decode()
+
+    udp_socket_server_file = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket_server_file.bind(("0.0.0.0", 4201))
+
+    
+    print(f"Enviando arquivo em:'{file_name}' para {client_address}")
+
+    # Abre o arquivo em modo binário para leitura
+    try:
+        with open(f"./files/{file_name}", "rb") as file:
+            while True:
+                # Lê o arquivo em blocos de 1024 bytes
+                print("Enviando stream...")
+                chunk = file.read(1024)
+                if not chunk:
+                    break
+
+                # Envia o bloco para o cliente
+                udp_socket_server_file.sendto(chunk, client_address)
+
+                try:
+                    ack, _ = udp_socket_server_file.recvfrom(1024)
+                    if ack != b"ACK":
+                        udp_socket_server_file.sendto(chunk, client_address)
+                        continue
+                except socket.timeout:
+                    print("Timeout aguardando ACK. Reenviando pacote.")
+                    udp_socket_server_file.sendto(chunk, client_address)
+            
+    except Exception as e:
+        print(f"Ocorreu um erro ao enviar o arquivo '{file_name}': {e}")
+
+    udp_socket_server_file.sendto(b"END_OF_FILE", client_address)
+    print(f"Envio do arquivo em:'{file_name}' concluído para {client_address}")
+    
 
 start_client()
---
+++ audio_stream_agent.py
@@ -0,0 +1,45 @@
+import socket
+import pyaudio
+
+# Definir configurações do PyAudio para a reprodução do áudio
+p = pyaudio.PyAudio()
+
+# Definir o formato do áudio (exemplo: mono, 16-bit, 44100Hz)
+FORMAT = pyaudio.paInt16  # 16 bits por amostra
+CHANNELS = 2  # Stereo (2 canais)
+RATE = 44100  # Taxa de amostragem (44.1 kHz)
+CHUNK = 1024  # Tamanho do bloco de áudio a ser lido
+
+# Abrir o stream de áudio para reprodução
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+# Criar o socket para escutar as conexões UDP
+udp_socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_socket_server.bind(('0.0.0.0', 4200))  # Escutando na porta 12345
+
+print("Aguardando dados de áudio...")
+
+while True:
+    # Receber dados de áudio em blocos de 1024 bytes
+    data, client_address = udp_socket_server.recvfrom(CHUNK)
+    print(f"Processando dados de: {client_address}")
+    
+    if not data:
+        break
+    
+    # Reproduzir o áudio assim que os dados são recebidos
+    stream.write(data)
+
+    udp_socket_server.sendto(b"ACK", client_address)
+
+
+# Finalizar o stream de áudio e fechar o PyAudio
+stream.stop_stream()
+stream.close()
+p.terminate()
+udp_socket_server.close()
+print("Reprodução de áudio concluída.")
